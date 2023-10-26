30744b0010d875df755749b37ddc26bb374d517a
John Michael Paraiso <johnmparaiso@gmail.com>
2023-10-27 00:21:36 +0100

    added player paddle script
    makes the player paddle move up and down

---
+++ Pong/Assets/Scripts/Paddle.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+//class inherited by computerpaddle and playerpaddle
+public class Paddle : MonoBehaviour
+{
+    protected Rigidbody2D _rigidbody;
+
+    public float speed = 10.0f;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+}
